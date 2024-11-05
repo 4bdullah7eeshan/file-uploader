436e55d69ae08a7187a1955a6a40a47774ef798d
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-05 18:00:58 +0530

    wip(ea): import necessary modules

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("node:path");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
+const { PrismaClient } = require('@prisma/client')
