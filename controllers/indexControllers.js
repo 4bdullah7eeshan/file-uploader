23ae002992d069326237ac3d4e8629c8c6738fc6
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-05 18:35:09 +0530

    feat(styles): add layout and resets

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const getHomePage = asyncHandler(async (req, res) => {
-    res.render("pages/index", { title: "Members Only" });
+    res.render("pages/index", { title: "File Uploader" });
 });
 
 
