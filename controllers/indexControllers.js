596a8a70197cdf169a7119a7151c66e0f8fe9d52
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-05 22:47:51 +0530

    fix: import prisma correctly and pass userId

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("express-async-handler");
-const prisma = require("../prisma/client");
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 const getHomePage = asyncHandler(async (req, res) => {
     const files = await prisma.file.findMany({
-        where: { fileId: req.user.id }, // change this later
+        where: { userId: req.user.id },
     });
 
     res.render("pages/index", { title: "File Uploader", user: req.user, files });
