b0513c9fb7bf66acca9d32768243fb1f85df237c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-05 22:16:00 +0530

    refactor: update controllers

--- controllers/uploadControllers.js
+++ controllers/uploadControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const getUploadPage = asyncHandler(async (req, res) => {
     res.render("pages/upload", { title: "Upload", user: req.user });
@@ -6,28 +9,32 @@ const getUploadPage = asyncHandler(async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/');
+        const uploadDir = path.join(__dirname, "../uploads");
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      const fileExtension = path.extname(file.originalname);
-      const baseName = path.basename(file.originalname, fileExtension);
-      cb(null, `${baseName}-${uniqueSuffix}${fileExtension}`);
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const fileExtension = path.extname(file.originalname);
+        const baseName = path.basename(file.originalname, fileExtension);
+        cb(null, `${baseName}-${uniqueSuffix}${fileExtension}`);
     }
 });
-  
-const upload = multer({ storage: storage });
+
+const upload = multer({ storage });
 
 const createFile = asyncHandler(async (req, res) => {
     if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
-    res.send(`File uploaded as: ${req.file.filename}`);
+        req.flash("error", "No file uploaded.");
+        return res.redirect("/upload");
+    }
+    res.render("pages/uploadSuccess", { filename: req.file.filename });
 });
 
 module.exports = {
     getUploadPage,
     createFile,
-
-
+    upload
 };
