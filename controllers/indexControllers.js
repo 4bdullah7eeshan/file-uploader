ec16f4c1af919be08e231c56a57d73f3553191f8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-05 22:37:28 +0530

    wip: fetch files from db

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const prisma = require("../prisma/client");
 
 const getHomePage = asyncHandler(async (req, res) => {
-    res.render("pages/index", { title: "File Uploader", user: req.user });
-});
+    const files = await prisma.file.findMany({
+        where: { fileId: req.user.id }, // change this later
+    });
 
+    res.render("pages/index", { title: "File Uploader", user: req.user, files });
+});
 
 module.exports = {
     getHomePage,
