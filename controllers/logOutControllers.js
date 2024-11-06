086c5d63ced371a1c9567bd6c8bf184d24c70e46
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-06 12:48:52 +0530

    feat: setup log out controllers

---
+++ controllers/logOutControllers.js
@@ -0,0 +1,19 @@
+const asyncHandler = require("express-async-handler");
+
+const logOut = asyncHandler(async (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.session.destroy((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect("/");
+        });
+    });
+});
+
+module.exports = {
+    logOut,
+};
